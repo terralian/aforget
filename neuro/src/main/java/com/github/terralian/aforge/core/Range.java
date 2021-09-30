869488a40b0ba39655631ad6abf3237ba5e59bc6
Terra.Lian <630985301@qq.com>
2021-09-30 17:37:28 +0800

    部分注释汉化

--- neuro/src/main/java/com/github/terralian/aforge/core/Range.java
+++ neuro/src/main/java/com/github/terralian/aforge/core/Range.java
@@ -8,42 +8,42 @@
 package com.github.terralian.aforge.core;
 
 /**
- * Represents a range with minimum and maximum values, which are single precision numbers (floats).
+ * 表示一个具有最小值与最大值的单精度(floats)区间.
  * <p>
- * The class represents a single precision range with inclusive limits - both minimum and maximum values of the range are included into it.
- * Mathematical notation of such range is <b>[min, max]</b>.
+ * 该类表示一个包含限制的单精度区间 - 内含最小值与最大值. 这个区间的数学符号是 <b>[min, max]</b>.
  * <p>
- * Sample usage:
+ * 样例:
+ * 
  * <pre>
- * // create [0.25, 1.5] range
+ * // 创建 [0.25, 1.5] 区间
  * Range range1 = new Range(0.25f, 1.5f);
- * // create [1.00, 2.25] range
+ * // 创建 [1.00, 2.25] 区间
  * Range range2 = new Range(1.00f, 2.25f);
- * // check if values is inside of the first range
+ * // 检查值是否在第一个区间内
  * if (range1.IsInside(0.75f)) {
  *     // ...
  * }
- * // check if the second range is inside of the first range
+ * // 检查第二个区间是否在第一个区间内
  * if (range1.IsInside(range2)) {
  *     // ...
  * }
- * // check if two ranges overlap
+ * // 检查两个区间是否存在重叠
  * if (range1.IsOverlapping(range2)) {
  *     // ...
  * }
  * </pre>
  */
 public class Range {
-    // Minimum value of the range.
+    // 区间最小值.
     private float min;
-    // Maximum value of the range.
+    // 区间最大值.
     private float max;
 
     /**
-     * Initializes a new instance of the {@link Range} structure.
+     * 初始化一个{@link Range}数据结构实例.
      * 
-     * @param min Minimum value of the range.
-     * @param max Maximum value of the range.
+     * @param min 区间最小值.
+     * @param max 区间最大值.
      */
     public Range(float min, float max) {
         this.min = min;
@@ -51,43 +51,44 @@ public class Range {
     }
 
     /**
-     * Check if the specified value is inside of the range.
+     * 检查指定的值是否在区间内.
      * 
-     * @param x Value to check.
-     * @return <b>True</b> if the specified value is inside of the range or <b>false</b> otherwise.
+     * @param x 要检查的值.
+     * @return <b>True</b> 表示在指定值在区间内，反之 <b>false</b>.
      */
     public boolean isInside(float x) {
         return ((x >= min) && (x <= max));
     }
 
     /**
-     * Check if the specified range is inside of the range.
+     * 检查指定的区间是否在区间内.
      * 
-     * @param range Range to check.
-     * @return <b>True</b> if the specified value is inside of the range or <b>false</b> otherwise.
+     * @param range 要检查的区间.
+     * @return <b>True</b> 表示指定的区间在区间内，反之 <b>false</b>.
      */
     public boolean isInside(Range range) {
         return ((isInside(range.min)) && (isInside(range.max)));
     }
 
     /**
-     * Check if the specified range overlaps with the range.
+     * 检查指定区间是否与该区间重叠（含有交集）.
      * 
-     * @param range Range to check for overlapping.
-     * @return <b>True</b> if the specified value is inside of the range or <b>false</b> otherwise.
+     * @param range 要检查的区间.
+     * @return <b>True</b> 表示指定区间与该区间重叠，反之 <b>false</b>.
      */
     public boolean isOverlapping(Range range) {
         return ((isInside(range.min)) || (isInside(range.max)) || (range.isInside(min)) || (range.isInside(max)));
     }
     
+    // 下面的这个方法需要引入一个新的类，由于没用到这里先去除.
     // ToIntRange
 
     /**
-     * Equality operator - checks if two ranges have equal min/max values.
+     * 检查两个区间是否相等（存在相同的最小值，最大值）.
      * 
-     * @param range1 First range to check.
-     * @param range2 Second range to check.
-     * @return <b>True</b> if min/max values of specified ranges are equal.
+     * @param range1 第一个要检查的区间.
+     * @param range2 第二个要检查的区间.
+     * @return <b>True</b> 表示两个区间的最大值及最小值相同.
      */
     public static boolean equals(Range range1, Range range2) {
         return ((range1.min == range2.min) && (range1.max == range2.max));
