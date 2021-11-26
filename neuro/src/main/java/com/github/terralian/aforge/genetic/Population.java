ffcf1181536b1437b12b1efe3ea4f59f55f0007f
Terra.Lian <630985301@qq.com>
2021-11-26 18:09:02 +0800

    Population类的部分汉化

--- neuro/src/main/java/com/github/terralian/aforge/genetic/Population.java
+++ neuro/src/main/java/com/github/terralian/aforge/genetic/Population.java
@@ -198,16 +198,14 @@ public class Population {
     /**
      * 对当前种群进行随机洗牌.
      * <p>
-     * Population shuffling may be useful in cases when selection
-     * operator results in not random order of chromosomes (for example, after elite
-     * selection population may be ordered in ascending/descending order).
+     * 该方法常用于 选择操作的结果导致的染色体顺序不是随机的情况（例如在选择精英染色体后，种群可能按升序/降序排列）
      */
     public void shuffle() {
-        // current population size
+        // 当前种群大小
         int size = population.size();
-        // create temporary copy of the population
+        // 创建种群的临时副本
         List<IChromosome> tempPopulation = population.subList(0, size);
-        // clear current population and refill it randomly
+        // 清空当前种群，并重新随机填充
         population.clear();
 
         while (size > 0) {
@@ -221,16 +219,13 @@ public class Population {
     }
 
     /**
-     * Add chromosome to the population.
+     * 将染色体添加到种群.
      * <p>
-     * The method adds specified chromosome to the current population.
-     * Manual adding of chromosome maybe useful, when it is required to add some initialized
-     * chromosomes instead of random.
+     * 该方法将指定的染色体添加到当前种群. 常用于添加某些已初始化的而非随机的染色体。
      * <p>
-     * Adding chromosome manually should be done very carefully, since it
-     * may break the population. The manually added chromosome must have the same type
-     * and initialization parameters as the ancestor passed to constructor.
-     * @param chromosome Chromosome to add to the population.
+     * 手动添加染色体应当非常小心，因为这可能破坏种群. 手动添加的染色体必须具有与初始化构造体时的祖先染色体一致的类型与初始化参数
+     * 
+     * @param chromosome 要添加到种群的染色体.
      */
     public void addChromosome(IChromosome chromosome) {
         chromosome.evaluate(fitnessFunction);
@@ -238,103 +233,95 @@ public class Population {
     }
 
     /**
-     * Perform migration between two populations.
+     * 在两个种群间执行迁移（migration）.
      * <p>
-     * The method performs migration between two populations - current and the anotherPopulation.
-     * During migration numberOfMigrants of chromosomes is choosen from each population using
-     * migrantsSelector and put into another population replacing worst members
-     * there.
+     * 该方法在两个种群之间执行迁移操作- 当前种群 和 另一个种群. 在迁移过程中，通过migrantsSelector从每个种群间选择numberOfMigrants个染色体，并将其放入另一个种群中，替换其中最差的个体。
      * 
-     * @param anotherPopulation Population to do migration with.
-     * @param numberOfMigrants Number of chromosomes from each population to migrate.
-     * @param migrantsSelector Selection algorithm used to select chromosomes to migrate.
+     * @param anotherPopulation 需要迁移的种群.
+     * @param numberOfMigrants 每个种群中需要迁移的数量.
+     * @param migrantsSelector 用于选择要迁移的染色体的选择算法.
      */
     public void migrate(Population anotherPopulation, int numberOfMigrants, ISelectionMethod migrantsSelector) {
         int currentSize = this.size;
         int anotherSize = anotherPopulation.size;
 
-        // create copy of current population
+        // 创建当前染色体的克隆
         List<IChromosome> currentCopy = new ArrayList<>();
 
         for (int i = 0; i < currentSize; i++) {
             currentCopy.add(population.get(i).clone());
         }
 
-        // create copy of another population
+        // 创建另一个染色体的克隆
         List<IChromosome> anotherCopy = new ArrayList<>();
 
         for (int i = 0; i < anotherSize; i++) {
             anotherCopy.add(anotherPopulation.population.get(i).clone());
         }
 
-        // apply selection to both populations' copies - select members to migrate
+        // 从各个种群执行选择算法 - 选择需要迁移的染色体
         migrantsSelector.applySelection(currentCopy, numberOfMigrants);
         migrantsSelector.applySelection(anotherCopy, numberOfMigrants);
 
-        // sort original populations, so the best chromosomes are in the beginning
+        // 对原始的种群进行排序，这样最好的染色体就在集合的开始部分
         population.sort((a, b) -> a.compareTo(b));
         anotherPopulation.population.sort((a, b) -> a.compareTo(b));
 
-        // remove worst chromosomes from both populations to free space for new members
+        // 从两个种群中剔除最差的染色体，为新成员腾出空间（从后向前删除N个元素）
         LangUtil.listRemoveRange(population, currentSize - numberOfMigrants, numberOfMigrants);
         LangUtil.listRemoveRange(anotherPopulation.population, anotherSize - numberOfMigrants, numberOfMigrants);
 
-        // put migrants to corresponding populations
+        // 添加移民染色体
         population.addAll(anotherCopy);
         anotherPopulation.population.addAll(currentCopy);
 
-        // find best chromosomes in each population
+        // 在每个种群中找到最好的染色体
         findBestChromosome();
         anotherPopulation.findBestChromosome();
     }
 
     /**
-     * Resize population to the new specified size.
+     * 调整种群的大小为目标值.
      * <p>
-     * The method does resizing of population. In the case if population should grow, it just adds missing number of random members. In the
-     * case if population should get smaller, the {@link #selectionMethod} is used to reduce the population.
+     * 该方法用于调整种群大小. 当种群需要增大，这里仅会增加缺少部分数量的随机染色体，当种群需要减小，这里会使用{@link #selectionMethod}选择算法来缩小种群。
      * 
-     * @param newPopulationSize New size of population.
-     * @throws IllegalArgumentException Too small population's size was specified. The exception is thrown in the case if newPopulationSize
-     *         is smaller than 2.
+     * @param newPopulationSize 新的种群数量.
+     * @throws IllegalArgumentException 指定的种群数量太小，若newPopulationSize小于2则会抛出该异常.
      */
     public void resize(int newPopulationSize) {
         resize(newPopulationSize, selectionMethod);
     }
 
     /**
-     * Resize population to the new specified size.
+     * 调整种群的大小为目标值.
      * <p>
-     * The method does resizing of population. In the case if population should grow, it just adds missing number of random members. In the
-     * case if population should get smaller, the specified selection method is used to reduce the population.
+     * 该方法用于调整种群大小. 当种群需要增大，这里仅会增加缺少部分数量的随机染色体，当种群需要减小，这里会使用membersSelector选择算法来缩小种群。
      * 
-     * @param newPopulationSize New size of population.
-     * @param membersSelector Selection algorithm to use in the case if population should get smaller.
-     * @throws IllegalArgumentException Too small population's size was specified. The exception is thrown in the case if newPopulationSize
-     *         is smaller than 2.
+     * @param newPopulationSize 新的种群数量.
+     * @param membersSelector 在缩小种群时所使用的选择算法
+     * @throws IllegalArgumentException 指定的种群数量太小，若newPopulationSize小于2则会抛出该异常.
      */
     public void resize(int newPopulationSize, ISelectionMethod membersSelector) {
         if (newPopulationSize < 2)
             throw new IllegalArgumentException("Too small new population's size was specified.");
 
         if (newPopulationSize > size) {
-            // population is growing, so add new rundom members
+            // 种群需要增长，所以这里需要增加新随机成员
 
-            // Note: we use population.Count here instead of "size" because
-            // population may be bigger already after crossover/mutation. So
-            // we just keep those members instead of adding random member.
+            // 注意：在这里使用种群，应使用差值而不是使用新大小，因为种群有可能已经过交叉/变异操作增大过了.
+            // 所以这里需要保持这些成员，仅添加缺失的数量，而非添加所有。
             int toAdd = newPopulationSize - population.size();
 
             for (int i = 0; i < toAdd; i++) {
-                // create new chromosome
+                // 创建新染色体
                 IChromosome c = population.get(0).createNew();
-                // calculate it's fitness
+                // 计算其适应度
                 c.evaluate(fitnessFunction);
-                // add it to population
+                // 添加到种群
                 population.add(c);
             }
         } else {
-            // do selection
+            // 进行选择
             membersSelector.applySelection(population, newPopulationSize);
         }
 
@@ -363,108 +350,88 @@ public class Population {
     }
 
     /**
-     * Crossover rate, [0.1, 1].
+     * 获得交叉概率, [0.1, 1].
      * <p>
-     * The value determines the amount of chromosomes which participate
+     * 该值决定参与交叉的染色体数量
      * <p>
-     * Default value is set to <b>0.75</b>.
+     * 默认值为 <b>0.75</b>.
      */
     public double getCrossoverRate() {
         return crossoverRate;
     }
 
     /**
-     * Crossover rate, [0.1, 1].
+     * 设置交叉概率, [0.1, 1].
      * <p>
-     * The value determines the amount of chromosomes which participate
-     * <p>
-     * Default value is set to <b>0.75</b>.
+     * 该值决定参与交叉的染色体数量
      * 
-     * @param crossoverRate value
+     * @param crossoverRate 值
      */
     public void setCrossoverRate(double crossoverRate) {
         this.crossoverRate = Math.max(0.1, Math.min(1.0, crossoverRate));
     }
 
     /**
-     * Mutation rate, [0.1, 1].
+     * 获得突变概率, [0.1, 1].
      * <p>
-     * The value determines the amount of chromosomes which participate in mutation.
+     * 该值决定参与突变的染色体数量
      * <p>
-     * Defaul value is set to <b>0.1</b>.
+     * 默认值为 <b>0.1</b>.
      */
     public double getMutationRate() {
         return mutationRate;
     }
 
     /**
-     * Mutation rate, [0.1, 1].
+     * 设置突变概率, [0.1, 1].
      * <p>
-     * The value determines the amount of chromosomes which participate in mutation.
-     * <p>
-     * Defaul value is set to <b>0.1</b>.
+     * 该值决定参与突变的染色体数量
      * 
-     * @param mutationRate value
+     * @param mutationRate 值
      */
     public void setMutationRate(double mutationRate) {
         this.mutationRate = Math.max(0.1, Math.min(1.0, mutationRate));
     }
 
     /**
-     * Random selection portion, [0, 0.9].
+     * 获取随机选择部分, [0, 0.9].
      * <p>
-     * The value determines the amount of chromosomes which will be
-     * randomly generated for the new population. The property controls the amount
-     * of chromosomes, which are selected to a new population using
-     * {@link #selectionMethod}, and amount of random
-     * chromosomes added to the new population.
+     * 该值确定为新种群随机生成的染色体数量. 该属性控制使用{@link #selectionMethod}选择到新种群的染色体数量
      * <p>
-     * Default value is set to <b>0</b>.
+     * 默认值为 <b>0</b>.
      */
     public double getRandomSelectionPortion() {
         return randomSelectionPortion;
     }
 
     /**
-     * Random selection portion, [0, 0.9].
+     * 设置随机选择部分, [0, 0.9].
      * <p>
-     * The value determines the amount of chromosomes which will be
-     * randomly generated for the new population. The property controls the amount
-     * of chromosomes, which are selected to a new population using
-     * {@link #selectionMethod}, and amount of random
-     * chromosomes added to the new population.
-     * <p>
-     * Default value is set to <b>0</b>.
+     * 该值确定为新种群随机生成的染色体数量. 该属性控制使用{@link #selectionMethod}选择到新种群的染色体数量
      * 
-     * @param randomSelectionPortion value
+     * @param randomSelectionPortion 值
      */
     public void setRandomSelectionPortion(double randomSelectionPortion) {
         this.randomSelectionPortion = Math.max(0, Math.min(0.9, randomSelectionPortion));
     }
 
     /**
-     * Determines of auto shuffling is on or off.
+     * 确定是否自动洗牌.
      * <p>
-     * The property specifies if automatic shuffling needs to be done
-     * on each {@link #runEpoch()} by calling {@link #shuffle()}
-     * method.
+     * 该属性确定是否需要在每个{@link #runEpoch()}上自动调用{@link #shuffle()}方法执行洗牌操作
      * <p>
-     * Default value is set to <see langword="false"/>.
+     * 默认值为 <b>false</b>.
      */
     public boolean isAutoShuffling() {
         return autoShuffling;
     }
 
     /**
-     * Determines of auto shuffling is on or off.
+     * 设置是否自动洗牌.
      * <p>
-     * The property specifies if automatic shuffling needs to be done
-     * on each {@link #runEpoch()} by calling {@link #shuffle()}
-     * method.
-     * <p>
-     * Default value is set to <see langword="false"/>.
+     * 该属性确定是否需要在每个{@link #runEpoch()}上自动调用{@link #shuffle()}方法执行洗牌操作
      * 
-     * @param autoShuffling value
+     * @param autoShuffling 值
      */
     public void setAutoShuffling(boolean autoShuffling) {
         this.autoShuffling = autoShuffling;
